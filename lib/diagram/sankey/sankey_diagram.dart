ea2a63ae778289b25c42f2ce0264ced06d8a4a88
junnikym <junnikym@gmail.com>
2024-07-21 23:34:53 +0900

    add: sankey diagram: node link painter

--- lib/diagram/sankey/sankey_diagram.dart
+++ lib/diagram/sankey/sankey_diagram.dart
@@ -49,6 +49,35 @@ class SankeyDiagramPainter extends CustomPainter {
     canvas.drawRRect(RRect.fromRectAndRadius(rect, radius), paint);
   }
 
+  void drawNodeLink(Canvas canvas, Offset leftNodePosition, Offset rightNodePoition) {
+    final offset = Offset(nodeSize.width / 2, nodeSize.height / 2);
+
+    final midPosition = Offset(
+      (leftNodePosition.dx + rightNodePoition.dx) / 2,
+      (leftNodePosition.dy + rightNodePoition.dy) / 2
+    );
+
+    Paint paint = Paint()
+      ..color = Colors.red
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = nodeSize.height;
+    
+    Path path = Path();
+    path.moveTo(
+      leftNodePosition.dx + offset.dx + nodeRadius, leftNodePosition.dy + offset.dy
+    );
+    path.quadraticBezierTo(
+      midPosition.dx + offset.dx, leftNodePosition.dy + offset.dy,
+      midPosition.dx + offset.dx, midPosition.dy + offset.dy
+    );
+    path.quadraticBezierTo(
+      midPosition.dx + offset.dx, rightNodePoition.dy + offset.dy,
+      rightNodePoition.dx + offset.dx - nodeRadius, rightNodePoition.dy + offset.dy
+    );
+
+    canvas.drawPath(path, paint);
+  }
+  
   @override
   void paint(Canvas canvas, Size size) {
   }
