807dc106a0edbc3c647c2c0af81036c6f13a575b
junnikym <junnikym@gmail.com>
2024-08-03 02:41:55 +0900

    edit: sankey diagram: more detailed amount

--- lib/diagram/sankey/sankey_diagram_node.dart
+++ lib/diagram/sankey/sankey_diagram_node.dart
@@ -1,14 +1,39 @@
+import 'package:uuid/uuid.dart';
+
 class SankeyDiagramNode {
 
-  String name;
+  static const Uuid uuidGenerator = Uuid();
 
-  List<SankeyDiagramNode> prev;
-  List<SankeyDiagramNode> next;
+  final id = uuidGenerator.v4();
 
-  SankeyDiagramNode(
-    this.name,
-    this.prev,
-    this.next
+  final String name;
+  final double amount = 0.0;
+
+  final List<SankeyDiagramNodeLinker> prev = [];
+  final List<SankeyDiagramNodeLinker> next = [];
+
+  SankeyDiagramNode(this.name);
+
+  void addPrev(SankeyDiagramNode node, double amount) {
+    prev.add(SankeyDiagramNodeLinker(node, amount));
+    amount += amount;
+  }
+
+  void addNext(SankeyDiagramNode node, double amount) {
+    next.add(SankeyDiagramNodeLinker(node, amount));
+    amount += amount;
+  }
+  
+}
+
+class SankeyDiagramNodeLinker {
+
+  final SankeyDiagramNode node;
+  final double amount;
+
+  SankeyDiagramNodeLinker (
+    this.node,
+    this.amount
   );
 
 }
